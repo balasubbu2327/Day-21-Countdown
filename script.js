b8e987904e4e41d0a40feb464d8dcce7e43f7f5e
Bala Subramanian <98680016+balasubbu2327@users.noreply.github.com>
2022-04-04 02:26:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+var i = 10;
+
+   setTimeout(function(){
+       document.getElementById('new').innerHTML = i;
+           setTimeout(function(){
+               i--;
+               document.getElementById('new').innerHTML = i;
+                    setTimeout(function(){
+                        i--;
+                         document.getElementById('new').innerHTML = i;
+                           setTimeout(function(){
+                             i--;
+                               document.getElementById('new').innerHTML = i;
+                                  setTimeout(function(){
+                                     i--;
+                                     document.getElementById('new').innerHTML = i;
+                                         setTimeout(function(){
+                                            i--;
+                                              document.getElementById('new').innerHTML = i;
+                                                setTimeout(function(){
+                                                 i--;
+                                                   document.getElementById('new').innerHTML = i;
+                                                     setTimeout(function(){
+                                                        i--;
+                                                      document.getElementById('new').innerHTML = i;
+                                                        setTimeout(function(){
+                                                            i--;
+                                                          document.getElementById('new').innerHTML = i;
+                                                             setTimeout(function(){
+                                                               i--;
+                                                                 document.getElementById('new').innerHTML = i;
+                                                                     setTimeout(function(){
+                                                                          i--;
+                                                                        document.getElementById('new').innerHTML = i;
+                                                                          setTimeout(function(){
+                                                                              i--;
+                                                                              document.getElementById('new').innerHTML = "Happy Independence Day";
+                                                                                  setTimeout(function(){
+                                                                                      i--;
+                                                                          },1000)
+                                                                    },1000)
+                                                             },1000)
+                                                         },1000)
+                                                       },1000)
+                                                  },1000)
+                                              },1000)
+
+                                         },1000)
+                         
+                                     },1000)
+                                 },1000)
+                             },1000)
+
+                           },1000)
+                      },1000)
